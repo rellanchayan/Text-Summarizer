3b8bb5354bba26db1ca30d628232ea38640e97a9
Chayan Rellan <rellanchayan@gmail.com>
2023-11-21 12:02:33 -0500

    Data Validation Notebook and Pipeline added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from TextSummarizer.logging import logger
 
 STAGE_NAME = 'Data Ingestion Stage'
@@ -12,4 +13,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = 'Data Validation Stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
